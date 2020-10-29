6af3576d9829f77596aa820d7ababa52ac581567
JaeYoung Chung <jychung0321@gmail.com>
2020-10-29 22:54:49 +0900

    first commit

---
+++ DLT.m
@@ -0,0 +1,25 @@
+function [H, minh, value] = DLT(numDLT, numMatch, mp1, mp2)
+    DLTrandIdx = randsample(numMatch, numDLT);
+    selpts1 = mp1(DLTrandIdx,:);
+    selpts2 = mp2(DLTrandIdx,:);
+    
+    A = [];
+    for i=1:numDLT
+        A = [A; [zeros(1,3),  -selpts1(i,:),  selpts2(i,2)*selpts1(i,:);
+                 selpts1(i,:),   zeros(1,3), -selpts2(i,1)*selpts1(i,:)]];
+    end
+
+    % DLT1 ?
+%     h = null(A);
+%     [minh, idxh] = min(vecnorm(A*h, 2));
+%     opth = h(:,idxh);
+
+    % DLT2 ?
+    [~,~,V] = svd(A);
+    opth = V(:,end);
+    minh = vecnorm(A*opth, 2);
+    
+    H = reshape(opth,3,3)';
+    
+    value = diag(selpts2*H*selpts1');
+end
---
+++ ICV_HW2.m
@@ -0,0 +1,53 @@
+clear; close all; clc;
+run('lib/vlfeat-0.9.21/toolbox/vl_setup.m')
+
+% data loading
+img = {};
+for i=1:6
+    tmpimg = imread('data/'+string(i)+'.jpg');
+    tmpimg = imresize(tmpimg(:,641:end-640,:), 0.2);
+    tmpimg = imrotate(tmpimg, -90);
+    img{7-i} = single(rgb2gray(tmpimg));
+end
+
+% getting SIFT features
+[fa, da] = vl_sift(img{1});
+[fb, db] = vl_sift(img{2});
+
+% plotting SIFT feature in one image
+figure(1); imshow(img{1}/255); hold on;
+perm = randperm(size(fa,2)) ;
+sel = perm(1:20) ;
+hkey = vl_plotframe(fa(:,sel)) ;
+hdes = vl_plotsiftdescriptor(da(:,sel),fa(:,sel)) ;
+set(hkey,'color','y','linewidth',2) ;
+set(hdes,'color','g') ;
+
+% plotting matched sift features
+[matches, scores] = vl_ubcmatch(da, db);
+matchedPoint1 = [fa(1:2, matches(1,:))', ones(size(matches,2),1)];
+matchedPoint2 = [fb(1:2, matches(2,:))', ones(size(matches,2),1)];
+figure(2); showMatchedFeatures(img{1}, img{2}, matchedPoint1(:,1:2), matchedPoint2(:,1:2), 'montage')
+
+%% DLT, RANSAC
+
+numIter = 50000;
+count = 0;
+while(count<=numIter)
+    [candidH, minh, value] = DLT(4, size(matches,2), matchedPoint1, matchedPoint2);
+    diag(matchedPoint2*candidH*matchedPoint1')
+
+    
+    numIter = numIter/10;
+    count = count + 1;
+end
+
+
+
+%%
+for i=1:6
+    figure(1);
+    imshow(img{i});
+    pause(0.5);
+end
+
